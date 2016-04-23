26f3121b1c239b755b6d05ff6d96e74b12ff6e13
morbius1st <morbius1st@cyberstudio.pro>
2016-04-23 06:27:16 -0700

    Initialize

---
+++ spec/colorguide-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ColorguideView from '../lib/colorguide-view';
+
+describe('ColorguideView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
